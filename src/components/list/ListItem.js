b275d96cdd5c1d8ee500578c34c2eaaeee3a8561
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-04-29 17:18:02 -0300

    working on todo input form

--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -1,12 +1,58 @@
 import React from "react";
-import {  Typography } from 'antd';
-import { AntDesignOutlined  } from '@ant-design/icons';
+import { Button, Input, Typography } from "antd";
+import { EditOutlined, AntDesignOutlined,SaveOutlined } from "@ant-design/icons";
+import { green, blue } from "@ant-design/colors";
+import { getState } from "redux";
+import { store } from "../store/store";
+import { useRef } from "react";
+import { useState } from "react";
 const ListItem = ({ id, todo }) => {
-
+  const { todo: todosFromStore } = store.getState();
+  const inputRef = useRef();
+  const [readOnly, setReadOnly] = useState(true);
+  const [saveVisibility, setSaveVisibility] = useState(true);
+  const [currentTodo] = todosFromStore.filter((t) => t.id === id);
+  // console.log(currentTodo);
+  const handleEdit = () => {
+    setReadOnly((prev) => !prev);
+    inputRef.current.focus();
+  };
+  const handleInputChange =()=>{
+    console.log("input change")
+     const {input : {value}} = inputRef.current;
+      
+     const {todo} = currentTodo 
+     setSaveVisibility( todo === value   )
+  }
+  const handleSave = ()=>{
+    console.log("save")
+  }
+  const readOnlyStyles = { border: "none" };
+  const activeEditStyles = { border: `1px solid ${blue[3]}` };
   return (
     <>
+      {" "}
       <AntDesignOutlined />
-      <Typography.Text>{todo}</Typography.Text>
+      <Input
+        readOnly={readOnly}
+        bordered={false}
+        onChange={handleInputChange}
+        defaultValue={todo}
+        style={{ transition:"border .4s ease",...readOnly ? readOnlyStyles : activeEditStyles}}
+        ref={inputRef}
+        onBlur = {()=>(setSaveVisibility((true)),(setReadOnly(true)))}
+      />
+      <Button
+        style={{ backgroundColor: green[4], color: "white" }}
+        onClick={handleEdit}
+        icon={<EditOutlined />}
+      />
+      <Button
+        style={{transition:"opacity .3s ease",...saveVisibility ? {opacity:0} :{ opacity:1} }}
+        onClick={handleSave}
+        type="primary"
+        icon={<SaveOutlined />}
+      />
     </>
   );
 };
