d63c504a5887792ac0ede211882f30039b9de63a
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-04-29 20:33:58 -0300

    working on todo input form

--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -7,26 +7,39 @@ import { store } from "../store/store";
 import { useRef } from "react";
 import { useState } from "react";
 const ListItem = ({ id, todo }) => {
+  const inputRef = useRef()
   const { todo: todosFromStore } = store.getState();
-  const inputRef = useRef();
+   
   const [readOnly, setReadOnly] = useState(true);
   const [saveVisibility, setSaveVisibility] = useState(true);
   const [currentTodo] = todosFromStore.filter((t) => t.id === id);
+  const [inputValue, setInputValue] = useState(currentTodo.todo)
   // console.log(currentTodo);
   const handleEdit = () => {
-    setReadOnly((prev) => !prev);
+    setReadOnly((prev) =>  !prev);
     inputRef.current.focus();
+    
   };
   const handleInputChange =()=>{
     console.log("input change")
      const {input : {value}} = inputRef.current;
       
-     const {todo} = currentTodo 
+     const {todo} = currentTodo
+     setInputValue(value) 
      setSaveVisibility( todo === value   )
   }
   const handleSave = ()=>{
     console.log("save")
   }
+  const handleOnBlur = ()=>{
+    console.log("blur")
+    // inputRef.current.input.value ="fodsfndsinin"
+    setInputValue(todo)
+  
+    setSaveVisibility(true);
+      setReadOnly(true);
+     
+    }
   const readOnlyStyles = { border: "none" };
   const activeEditStyles = { border: `1px solid ${blue[3]}` };
   return (
@@ -37,10 +50,11 @@ const ListItem = ({ id, todo }) => {
         readOnly={readOnly}
         bordered={false}
         onChange={handleInputChange}
-        defaultValue={todo}
+        value={inputValue}
+
         style={{ transition:"border .4s ease",...readOnly ? readOnlyStyles : activeEditStyles}}
         ref={inputRef}
-        onBlur = {()=>(setSaveVisibility((true)),(setReadOnly(true)))}
+        onBlur={handleOnBlur}
       />
       <Button
         style={{ backgroundColor: green[4], color: "white" }}
