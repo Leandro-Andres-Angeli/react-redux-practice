cde42b5d48d066fdc10939876fffd2e98051480a
Leandro_Angeli <leandroandresangeli@gmail.com>
2023-04-30 16:14:02 -0300

    working on UI with reducers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
+ 
  
 } from "react-router-dom";
 import Navbar from './components/Navbar';
--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,19 +1,67 @@
-import { Divider, List, Typography } from "antd";
+import { green, red } from "@ant-design/colors";
+import { Button, Divider, Form, Input, List, Typography } from "antd";
 
-import React from "react";
+import React, { useReducer } from "react";
 import { store } from "../store/store";
+const uiState = {
+  editMode:false,
+}
+const resetUiActions = ()=>uiState
+const types = {
+  toggleEditMode:'edit mode one'
+}
+const uiReducer = (state,action)=>{
+  switch (action.type){
+    case types.toggleEditMode :
+    return {...state,editMode: !state.editMode  }
+    default:
+      return state
+  }
+  
 
+}
 const UsersListItem = ({ user }) => {
+  const [uiActions , dispatchUiActions] = useReducer(uiReducer,uiState,resetUiActions)
+  // return (
+  //   <List.Item>
+  //     <List
+     
+  //   dataSource={Object.entries(user)}
+  //       renderItem={ ([field,data])=> (
+  //         <List.Item >{field} : {data}</List.Item>
+  //       )}
+  //     />
+
+    
+  //   </List.Item>
+  // );
+  const {editMode} = uiActions
+  const handleSubmit=()=>{
+    console.log("sub")
+  }
+  const handleEdit = ()=>{
+    console.log('edit')
+    dispatchUiActions({type : types.toggleEditMode})
+    
+  }
   return (
     <List.Item>
-      <List
-     
-    dataSource={Object.entries(user)}
-        renderItem={ ([field,data])=> (
-          <List.Item >{field} : {data}</List.Item>
-        )}
-      />
-
+      <div> {JSON.stringify(uiActions)}</div>
+    
+       <Form onFinish={handleSubmit}> 
+        <fieldset style={{border:"none"}} disabled = {!editMode}>
+        {Object.entries(user).filter(([field])=> field !== "id").map(([field,value])=>
+      ( <Form.Item key={field} label={field} >
+         <Input defaultValue={value} />
+      </Form.Item>
+       ))}
+       </fieldset>
+       
+       <Button type="primary"   disabled = {!editMode} htmlType="submit">Submit</Button>
+       <Button style={{backgroundColor:green[3],color:"white"}}  onClick={handleEdit} >Edit</Button>
+       <Button  danger >Delete</Button>
+       
+       </Form>
     
     </List.Item>
   );
